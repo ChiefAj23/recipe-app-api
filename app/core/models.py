e7bcde92676048e5dfe1ef8cb2741e88241979a6
chiefaj <abhijeet.solanki@outlook.com>
2023-10-06 14:21:54 -0500

    Created User Model for Recipe-api, configured Django to use it, Handle normalising email, and handle encrypting password

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models # noqa
+"""
+Database models for the core app.
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser with given details."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True) # is_active is a user
+    is_staff = models.BooleanField(default=False) # is_staff is a superuser
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email' # this is the default, but we're being explicit
